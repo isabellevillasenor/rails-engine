d5ea99131d7a81a7a99e2445dd982765a9a609d8
Isabelle Villasenor <villasenori@outlook.com>
2021-02-17 07:24:56 -0700

    Add Items Relationship to Merchant Model and Model Spec

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,6 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name
+  has_many :items
 
   scope :search_all_by_name, ->(search) {where 'name LIKE :search', search: "%#{search.downcase}%"}
 end
