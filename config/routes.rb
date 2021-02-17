7896a40c48a9fb85fc8f7d1327abb4c9fecf5232
Isabelle Villasenor <villasenori@outlook.com>
2021-02-17 08:04:30 -0700

    Add Item Search Route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
       namespace :merchants do
         get '/find_all', to: 'search#index'
       end
+
+      namespace :items do
+        get 'find', to: 'search#show'
+      end
     end
   end
 end
