0ecb2dea6f03791c6a32ef4b2efe2c8b37b34ed5
Isabelle Villasenor <villasenori@outlook.com>
2021-02-17 08:05:13 -0700

    Add Item Find One By Name Scope to Model

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ApplicationRecord
                         :description
   validates :unit_price, numericality: { greater_than: 0 }
   belongs_to :merchant
+
+  scope :search_by_name, ->(search) {find_by 'name LIKE :search', search: "%#{search.downcase}%"}
 end
