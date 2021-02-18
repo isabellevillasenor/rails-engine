c13ff6b877d64753b58ddb100633e2c006c7e5d5
Isabelle Villasenor <villasenori@outlook.com>
2021-02-17 21:08:35 -0700

    Add Destroy Item Route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
         get '/find_all', to: 'search#index'
       end
       
-      resources :items, only: [:index, :show, :create, :update]
+      resources :items, only: [:index, :show, :create, :update, :destroy]
       
       namespace :items do
         get 'find', to: 'search#show'
