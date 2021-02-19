ea01b711077ed8dc53f624cbcf026e6682b47f63
Isabelle Villasenor <villasenori@outlook.com>
2021-02-19 07:12:39 -0700

    Add Most Items to Merchants Logic Controller

--- app/controllers/api/v1/merchants/logic_controller.rb
+++ app/controllers/api/v1/merchants/logic_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::Merchants::LogicController < ApplicationController
   def most_revenue
     render json: MerchantSerializer.new(Merchant.most_revenue(params[:quantity]))
   end
+
+  def most_items
+    render json: MerchantSerializer.new(Merchant.most_items(params[:quantity]))
+  end
 end
