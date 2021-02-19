68bf7eba38f5aec6f97abb61916834aaff760ec8
Isabelle Villasenor <villasenori@outlook.com>
2021-02-18 20:40:33 -0700

    Add Relationships to Merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,7 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name
   has_many :items
+  has_many :invoices
 
   scope :search_all_by_name, ->(search) {where 'name LIKE :search', search: "%#{search.downcase}%"}
   scope :search_one_by_name, ->(search) {find_by 'name LIKE :search', search: "%#{search.downcase}%"}
