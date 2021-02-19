8a16146e90dded9c436aea74d361ce9b3d42e0a5
Isabelle Villasenor <villasenori@outlook.com>
2021-02-18 18:37:55 -0700

    Add Item Find All Index Controller Method

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::Items::SearchController < ApplicationController
+  def index
+    items = Item.search_all_by_name(params[:name])
+    render json: ItemSerializer.new(items)
+  end
+
   def show
-    item = Item.search_by_name(params[:name])
+    item = Item.order(name: :asc).search_one_by_name(params[:name])
     render json: ItemSerializer.new(item)
   end
 end
