ce74ce04a7572e3bfc5c51d00dc5c182f7e43e3b
Isabelle Villasenor <villasenori@outlook.com>
2021-02-18 18:12:05 -0700

    Add Find One Merchant Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, except: [:new, :edit]
       
       namespace :merchants do
         get '/:id/items', to: 'items#index'
         get '/find_all', to: 'search#index'
+        get '/find_one', to: 'search#show'
       end
       
+      resources :merchants, except: [:new, :edit]
       resources :items, except: [:new, :edit]
       
       namespace :items do
