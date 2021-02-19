7fabd42c89a61d86027d50dfd03b639086643ae1
Isabelle Villasenor <villasenori@outlook.com>
2021-02-18 20:36:18 -0700

    Add Invoice to Schema, Model and Model Spec

---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Invoice do
+  describe 'validations' do
+    it { should validate_presence_of :status }
+    it { should validate_presence_of :merchant_id }
+    it { should validate_presence_of :customer_id }
+  end
+  describe 'relationships' do
+    it { should belong_to :merchant }
+    it { should belong_to :customer }
+  end
+end
