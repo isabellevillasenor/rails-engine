64ede84ec19c2701a42f8fe569beb4925e848ae9
Isabelle Villasenor <villasenori@outlook.com>
2021-02-18 18:12:18 -0700

    Add Find One Merchant Scope

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,5 @@ class Merchant < ApplicationRecord
   has_many :items
 
   scope :search_all_by_name, ->(search) {where 'name LIKE :search', search: "%#{search.downcase}%"}
+  scope :search_one_by_name, ->(search) {find_by 'name LIKE :search', search: "%#{search.downcase}%"}
 end
