5f1b0de5f4bc2e01901f79797cc9b91a6d74ff67
Isabelle Villasenor <villasenori@outlook.com>
2021-02-18 20:41:18 -0700

    Add Customer Validations and Relationships to Model Spec

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,4 +5,8 @@ describe Customer do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
   end
+
+  describe 'relationships' do
+    it { should have_many :invoices }
+  end
 end
