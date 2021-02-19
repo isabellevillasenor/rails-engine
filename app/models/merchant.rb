80ff80bc547315f8f3e4c1858d29a57c651f40fe
Isabelle Villasenor <villasenori@outlook.com>
2021-02-19 06:49:59 -0700

    Add Most Revenue Merchant Model Method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,4 +5,13 @@ class Merchant < ApplicationRecord
 
   scope :search_all_by_name, ->(search) {where 'name LIKE :search', search: "%#{search.downcase}%"}
   scope :search_one_by_name, ->(search) {find_by 'name LIKE :search', search: "%#{search.downcase}%"}
+
+  def self.most_revenue(quantity)
+    joins(invoices: [:invoice_items, :transactions])
+    .where("invoices.status='shipped' AND transactions.result='success'")
+    .select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+    .group('merchants.id')
+    .order('revenue DESC')
+    .limit(quantity.to_i)
+  end
 end
